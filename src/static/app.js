3aadec551e45241511452b85d35b38ac2c22c96c
arpitsharma9 <arpit.sharma9@gmail.com>
2025-11-09 14:45:48 +0000

    Fix HTML structure and correct availability label in activity cards

--- src/static/app.js
+++ src/static/app.js
@@ -42,11 +42,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         activityCard.innerHTML = `
           <h4>${name}</h4>
-          h4>"this is a test"/h4>
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Availabilit2y:</strong> ${spotsLeft} spots left</p>
           ${participantsHTML}
         `;
 
